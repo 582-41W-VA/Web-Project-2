873575d36f9902c2e6e59b79b66b86336761a2ee
Linlin Zhu <1695420@edu.vaniercollege.qc.ca>
2025-02-03 07:57:21 -0500

    add login, register and logout functions

--- brainboosters/urls.py
+++ brainboosters/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
 
 if settings.DEBUG:
